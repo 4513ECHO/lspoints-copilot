0e285c771beecb287fbf1402d51d99db5699795c
4513ECHO <mail@4513echo.dev>
2024-09-03 03:40:49 +0900

    fix: Fix accept

--- denops/@lspoints/copilot.ts
+++ denops/@lspoints/copilot.ts
@@ -167,20 +167,15 @@ async function getDisplayAdjustment(
   const selectedText = line.substring(0, candidate.range.start.character) +
     candidate.insertText.replace(/\n*$/, "");
   const typed = line.substring(0, offset);
-  const endOffset = line.length > candidate.range.end.character
-    ? candidate.range.end.character
-    : line.length;
-  const toDelete = line.substring(offset, endOffset + 1);
+  const endOffset = Math.min(line.length, candidate.range.end.character);
+  const toDelete = line.substring(offset, endOffset);
   if (typed.trim() === "") {
-    const leading = selectedText.match(/^\s*/)?.[0] ?? "";
+    const leading = selectedText.match(/^\s+/)?.[0] ?? "";
     const unindented = selectedText.trimStart();
     if (
       typed.substring(0, leading.length) === leading && unindented !== toDelete
     ) {
-      const indent = typed.length - leading.length;
-      const shiftwidth = await denops.call("shiftwidth") as number;
-      const outdent = Math.floor(indent / shiftwidth) + indent % shiftwidth;
-      return [unindented, outdent, toDelete.length];
+      return [unindented, typed.length - leading.length, toDelete.length];
     }
   } else if (typed === selectedText.substring(0, offset)) {
     return [selectedText.substring(offset), 0, toDelete.length];
@@ -325,28 +320,28 @@ export class Extension extends BaseExtension {
           return;
         }
         await denops.cmd("unlet! b:__copilot");
-        const replaced = pattern
+        const newText = pattern
           ? text.match("\n*(?:" + pattern + ")")?.[0].replace(/\n*$/, "") ??
             text
           : text;
-        if (text === replaced) {
+        if (text === newText) {
           await client.request("workspace/executeCommand", candidate.command);
         } else {
           const [line, col] = await batch.collect(denops, (denops) => [
             denops.call("getline", "."),
             denops.call("col", "."),
           ]) as [string, number];
+          const lineText = line.substring(0, col - 1) + newText;
           await client.notify("textDocument/didPartiallyAcceptCompletion", {
             item: candidate,
-            acceptedLength: (line.substring(0, col - 1) + replaced).length -
-              candidate.range.start.character,
+            acceptedLength: lineText.length - candidate.range.start.character,
           });
         }
         await clearPreview(denops);
         await send(denops, [
-          rawString`${"\\<BS>".repeat(outdent)}`,
+          rawString`${"\\<Left>\\<Del>".repeat(outdent)}`,
           rawString`${"\\<Del>".repeat(toDelete)}`,
-          rawString`${text.replaceAll("\n", "\n\\<C-u>")}`,
+          rawString`\<Cmd>set paste\<CR>${newText}\<Cmd>set nopaste\<CR>`,
           ...(pattern ? [] : [rawString`\<End>`]),
         ]);
       },
